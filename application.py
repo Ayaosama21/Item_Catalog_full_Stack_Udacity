e54116691acc17c8c06f10faef32550f719821f6
Ayaosama21 <ayao21684@gmail.com>
2019-10-02 21:25:41 +0200

    update application

--- application.py
+++ application.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import (Flask,
+                   render_template, 
+                   request, 
+                   redirect, 
+                   jsonify, 
+                   url_for, 
+                   flash)
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import desc
@@ -46,10 +52,10 @@ def fbconnect():
     print "access token received %s " % access_token
 
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+    app_id = json.loads(open('fb_client.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open('fb_client.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_++secret, access_token)
     h = httplib2.Http()
