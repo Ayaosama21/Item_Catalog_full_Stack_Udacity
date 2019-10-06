184a8d5ea5ca75a47df939fd59d10d9340d37a08
Ayaosama21 <ayao21684@gmail.com>
2019-10-06 21:18:26 +0200

    init commit

--- application.py
+++ application.py
@@ -220,22 +220,22 @@ def gconnect():
 
 
 def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session[
+    newUser = USER(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
     session.add(newUser)
     session.commit()
-    user = session.query(User).filter_by(email=login_session['email']).one()
+    user = session.query(USER).filter_by(email=login_session['email']).one()
     return user.id
 
 
 def getUserInfo(user_id):
-    user = session.query(User).filter_by(id=user_id).one()
+    user = session.query(USER).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
     try:
-        user = session.query(User).filter_by(email=email).one()
+        user = session.query(USER).filter_by(email=email).one()
         return user.id
     except:
         return None
